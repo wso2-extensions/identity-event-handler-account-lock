e6b330e3cd8b6bb7f31282189bf3027fdf37bfad
malithie <malithi.eamm@gmail.com>
2018-01-07 21:08:24 +0530

    Add account lock service that can be invoked to check account lock state.

---
+++ components/org.wso2.carbon.identity.handler.event.account.lock/src/main/java/org/wso2/carbon/identity/handler/event/account/lock/service/AccountLockService.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations und
+ */
+
+package org.wso2.carbon.identity.handler.event.account.lock.service;
+
+import org.wso2.carbon.identity.handler.event.account.lock.exception.AccountLockServiceException;
+
+/**
+ * Service interface that returns account lock states.
+ */
+public interface AccountLockService {
+
+    /**
+     * Returns if account is locked or not as a boolean.
+     * <p>
+     * Account is considered to be unlocked, if account lock claim (http://wso2.org/claims/identity/accountLocked) is
+     * set to 'false' or, if account unlock time claim (http://wso2.org/claims/identity/unlockTime) is not zero and
+     * current time exceeds account unlock time claim value when account lock claim is set to 'true'.
+     * Unless if account lock claim is set to 'true' account is always considered to be locked.
+     *
+     * @param domainAwareUsername username with user store domain
+     * @param tenantDomain        tenant domain of the user
+     * @return true if the account is locked and false if the account is unlocked
+     */
+    boolean isAccountLocked(String domainAwareUsername, String tenantDomain) throws AccountLockServiceException;
+
+    /**
+     * Returns if account is locked or not as a boolean.
+     * <p>
+     * Account is considered to be unlocked, if account lock claim (http://wso2.org/claims/identity/accountLocked) is
+     * set to 'false' or, if account unlock time claim (http://wso2.org/claims/identity/unlockTime) is not zero and
+     * current time exceeds account unlock time claim value when account lock claim is set to 'true'.
+     * Unless if account lock claim is set to 'true' account is always considered to be locked.
+     *
+     * @param username        username without user store domain
+     * @param tenantDomain    tenant domain of the user
+     * @param userStoreDomain user store domain of the user
+     * @return
+     * @throws AccountLockServiceException
+     */
+    boolean isAccountLocked(String username, String tenantDomain, String userStoreDomain) throws
+            AccountLockServiceException;
+}
---
+++ components/org.wso2.carbon.identity.handler.event.account.lock/src/main/java/org/wso2/carbon/identity/handler/event/account/lock/service/AccountLockServiceImpl.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations und
+ */
+
+package org.wso2.carbon.identity.handler.event.account.lock.service;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.wso2.carbon.base.MultitenantConstants;
+import org.wso2.carbon.identity.core.util.IdentityUtil;
+import org.wso2.carbon.identity.handler.event.account.lock.constants.AccountConstants;
+import org.wso2.carbon.identity.handler.event.account.lock.exception.AccountLockServiceException;
+import org.wso2.carbon.identity.handler.event.account.lock.internal.AccountServiceDataHolder;
+import org.wso2.carbon.user.api.UserRealm;
+import org.wso2.carbon.user.api.UserStoreException;
+import org.wso2.carbon.user.api.UserStoreManager;
+import org.wso2.carbon.user.core.UserCoreConstants;
+
+import java.util.Map;
+
+/**
+ * Service implementation class of {@link AccountLockService} that returns account lock states.
+ */
+public class AccountLockServiceImpl implements AccountLockService {
+
+    @Override
+    public boolean isAccountLocked(String domainAwareUsername, String tenantDomain) throws AccountLockServiceException {
+
+        UserStoreManager userStoreManager = getUserStoreManager(tenantDomain);
+        boolean accountLocked = getAccountLockClaimValue(domainAwareUsername, userStoreManager);
+        if (accountLocked) {
+            long accountUnlockTime = getAccountUnlockTimeClaimValue(domainAwareUsername, userStoreManager);
+            if (accountUnlockTime != 0 && System.currentTimeMillis() >= accountUnlockTime) {
+                return false;
+            }
+        }
+        return accountLocked;
+    }
+
+    @Override
+    public boolean isAccountLocked(String username, String tenantDomain, String userStoreDomain) throws
+            AccountLockServiceException {
+
+        return isAccountLocked(IdentityUtil.addDomainToName(username, userStoreDomain), tenantDomain);
+    }
+
+    private boolean getAccountLockClaimValue(String username, UserStoreManager userStoreManager) throws
+            AccountLockServiceException {
+
+        String accountLockedClaimValue = getClaimValue(username, userStoreManager, AccountConstants
+                .ACCOUNT_LOCKED_CLAIM);
+        return Boolean.parseBoolean(accountLockedClaimValue);
+    }
+
+    private long getAccountUnlockTimeClaimValue(String username, UserStoreManager userStoreManager) throws
+            AccountLockServiceException {
+
+        long unlockTime = 0;
+        String accountUnlockTimeClaimValue = getClaimValue(username, userStoreManager, AccountConstants
+                .ACCOUNT_UNLOCK_TIME_CLAIM);
+        if (NumberUtils.isNumber(accountUnlockTimeClaimValue)) {
+            unlockTime = Long.parseLong(accountUnlockTimeClaimValue);
+        }
+
+        return unlockTime;
+    }
+
+    private String getClaimValue(String username, UserStoreManager userStoreManager, String claimURI) throws
+            AccountLockServiceException {
+
+        try {
+            Map<String, String> values = userStoreManager.getUserClaimValues(username, new String[]{claimURI},
+                    UserCoreConstants.DEFAULT_PROFILE);
+            return values.get(claimURI);
+
+        } catch (UserStoreException e) {
+            throw new AccountLockServiceException("Error occurred while retrieving claim: " + claimURI, e);
+        }
+    }
+
+    private UserStoreManager getUserStoreManager(String tenantDomain) throws AccountLockServiceException {
+
+        int tenantId;
+        try {
+            tenantId = AccountServiceDataHolder.getInstance().getRealmService().getTenantManager().getTenantId
+                    (tenantDomain);
+        } catch (UserStoreException e) {
+            throw new AccountLockServiceException("Could not retrieve tenant id from tenant domain: " + tenantDomain,
+                    e);
+        }
+
+        if (MultitenantConstants.INVALID_TENANT_ID == tenantId) {
+            throw new AccountLockServiceException("Invalid tenant domain: " + tenantDomain);
+        }
+
+        UserRealm userRealm;
+        try {
+            userRealm = AccountServiceDataHolder.getInstance().getRealmService().getTenantUserRealm(tenantId);
+        } catch (UserStoreException e) {
+            throw new AccountLockServiceException("Could not retrieve user realm for tenant domain: " + tenantDomain,
+                    e);
+        }
+
+        try {
+            return userRealm.getUserStoreManager();
+        } catch (UserStoreException e) {
+            throw new AccountLockServiceException("Could not retrieve user store for tenant domain: " + tenantDomain,
+                    e);
+        }
+    }
+}
