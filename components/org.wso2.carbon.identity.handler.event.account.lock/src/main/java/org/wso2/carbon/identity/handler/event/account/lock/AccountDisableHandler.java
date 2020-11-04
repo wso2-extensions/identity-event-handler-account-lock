d35e4eb2ae52ae969a71764f6942f59de9f996c6
piraveena <piraveena18@gmail.com>
2020-11-04 18:51:51 +0530

    Fix the issue with token revocation when account is locked/disabled

--- components/org.wso2.carbon.identity.handler.event.account.lock/src/main/java/org/wso2/carbon/identity/handler/event/account/lock/AccountDisableHandler.java
+++ components/org.wso2.carbon.identity.handler.event.account.lock/src/main/java/org/wso2/carbon/identity/handler/event/account/lock/AccountDisableHandler.java
@@ -234,6 +234,9 @@ public class AccountDisableHandler extends AbstractEventHandler implements Ident
                     IdentityUtil.setIdentityErrorMsg(customErrorMessageContext);
                     IdentityUtil.threadLocalProperties.get().put(IdentityCoreConstants.USER_ACCOUNT_STATE,
                             IdentityCoreConstants.USER_ACCOUNT_DISABLED_ERROR_CODE);
+                    IdentityUtil.threadLocalProperties.get()
+                            .put(AccountConstants.USER_ACCOUNT_STATE_WITH_USERNAME + userName,
+                                    IdentityCoreConstants.USER_ACCOUNT_DISABLED_ERROR_CODE);
                 }
             } else {
                 if (existingAccountDisabledValue) {
